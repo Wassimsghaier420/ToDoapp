0917008f37ed910c4b150697d58c8c5ea0f8122b
Wassimsghaier420 <52701776+Wassimsghaier420@users.noreply.github.com>
2019-10-11 11:31:26 +0100

    Updates

---
+++ precache-manifest.17fda72d66a70b754b3c1ee63bd1bf5d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "992465551a1805688ce3dcf23414faa6",
+    "url": "/ToDoapp/index.html"
+  },
+  {
+    "revision": "7600dce6fdc8bdd055ec",
+    "url": "/ToDoapp/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "8ae3905278fa5b93cb5e",
+    "url": "/ToDoapp/static/js/2.454cdbe1.chunk.js"
+  },
+  {
+    "revision": "7600dce6fdc8bdd055ec",
+    "url": "/ToDoapp/static/js/main.dce542e0.chunk.js"
+  },
+  {
+    "revision": "d067dac17613a0c322ee",
+    "url": "/ToDoapp/static/js/runtime-main.fc07025a.js"
+  }
+]);
